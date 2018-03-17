93b4ae6458bec928a0fde8060607934d27ccbce4
Nacho <nacho@air>
2018-03-17 11:33:16 +0100

    Add package structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # datos.gov.es.r
 Package to access programmatically datos.gov.es
+
+
---
+++ utils/str.R
@@ -0,0 +1,8 @@
+#workflow to create a package to access datos.gob.es
+
+#create package structure
+
+library(usethis)
+create_package(path = getwd())
+
+#

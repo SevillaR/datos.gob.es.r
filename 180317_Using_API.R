8e072bacb1bb23f5a4e853549332ef7103642634
VictorSLledo <vsslledo@gmail.com>
2018-03-17 13:45:01 +0100

    Script para usar la API de datos.gob.es

---
+++ 180317_Using_API.R
@@ -0,0 +1,22 @@
+library(httr)
+library(jsonlite)
+url <- "http://datos.gob.es/apidata/catalog/dataset"
+
+# Make the GET request
+request_result_a <- GET(url, user_agent("vsslledo@gmail.com This is a test"))
+
+
+if(http_error(request_result_a)){
+  warning("The request failed")
+} else {
+  message(http_type(request_result_a))
+  request_result <- content(request_result_a, as = "parsed")
+  # return(request_result)
+}
+
+df <- fromJSON(content(request_result_a, as = "text"))
+df$keyword
+
+df <- df$result
+
+df <- df$items

9cd5329d8eeef6aa8aa3031410a266b019fd460a
Nacho <nacho@air>
2018-03-17 11:40:49 +0100

    Add code of conduct.

--- utils/str.R
+++ utils/str.R
@@ -5,4 +5,13 @@
 library(usethis)
 create_package(path = getwd())
 
-#
+use_code_of_conduct()
+#added a MIT licence manually.
+
+#add travis et al.
+#use_travis() #not done yet.
+#use_coverage()
+
+#add tests
+#use_testthat() #not done yet
+
